126882a7771e6cf1a7458cecdf355af2d9f567ee
Euler-JS <joaoalbertojose96@gmail.com>
2021-12-21 17:56:31 +0200

    set payment by API

--- assets/dist/js/getdata.js
+++ assets/dist/js/getdata.js
@@ -4,6 +4,7 @@ let letras = ["A", "B", "C", "D", "Z", "XC", "SA", "SW", "D", "EW", "PO", "KL", 
 let todosProdutos = [];
 let todosProdutosFiltros = [];
 var workerSelecionado;
+let meuCarinho;
 function login2() {
 
 
@@ -45,7 +46,7 @@ firebase.auth().onAuthStateChanged(function (user) {
         else {
             console.log("verificado");
 
-
+            getCarinho()
         }
 
     }
@@ -114,6 +115,7 @@ function adicionarProduto() {
         imagem_principal: "url1",
         outras_imagens: "url2",
         visualizacoes: '11',
+        carinho:""
 
     }
 
@@ -252,9 +254,9 @@ criarConteudoDeProduto = function (element)
                     <p class="vote"> <strong>Total no stock (`+element.total_unidades+`)</strong></p>
                     
                     
-                    <div class="action">
-                        <button class="add-to-cart btn btn-default" type="button">Adicionar ao Carinho</button>
-                        <button class="like btn btn-default" type="button"><span class="fa fa-heart"></span></button>
+                    <div class="action" id="pagamento">
+                        
+                        
                     </div>
                 </div>
             </div>
@@ -265,6 +267,179 @@ criarConteudoDeProduto = function (element)
 </div>
 
     `);
+
+    let button_pagar = $('<button data-toggle="modal" data-target="#exampleModalLong-pagamentos" class="add-to-cart btn btn-default" type="button">Efectuar pagamento</button>');
+
+
+    //Para adicionar botao com funcionalidade
+    button_pagar.on('click', () => {
+       criarConteudoDePagemnto(element)
+        
+    });
+
+    // tes.append(button_pagar);
+
+
+    $('#pagamento').append(button_pagar);
+}
+
+criarConteudoDePagemnto = function (element)
+{
+    $('#conteudo-modal-pagamentos').append(`
+    <div class="container py-5">
+    <!-- For demo purpose -->
+    <div class="row mb-4">
+        <div class="col-lg-8 mx-auto text-center">
+            <h1 class="display-6">Bootstrap Payment Forms</h1>
+        </div>
+    </div> <!-- End -->
+    <div class="row">
+        <div class="col-lg-6 mx-auto">
+            <div class="card ">
+                <div class="card-header">
+                    <div class="bg-white shadow-sm pt-4 pl-2 pr-2 pb-2">
+                        <!-- Credit card form tabs -->
+                        <ul role="tablist" class="nav bg-light nav-pills rounded nav-fill mb-3">
+                            <li class="nav-item"> <a data-toggle="pill" href="#credit-card" class="nav-link active "> <i class="fas fa-credit-card mr-2"></i> Credit Card </a> </li>
+                            <li class="nav-item"> <a data-toggle="pill" href="#paypal" class="nav-link "> <i class="fab fa-paypal mr-2"></i> Paypal </a> </li>
+                            <li class="nav-item"> <a data-toggle="pill" href="#net-banking" class="nav-link "> <i class="fas fa-mobile-alt mr-2"></i> MPesa </a> </li>
+                        </ul>
+                    </div> <!-- End -->
+                    <!-- Credit card form content -->
+                    <div class="tab-content">
+                        <!-- credit card info-->
+                        <div id="credit-card" class="tab-pane fade show active pt-3">
+                            <form role="form" onsubmit="event.preventDefault()">
+                                <div class="form-group"> <label for="username">
+                                        <h6>Card Owner</h6>
+                                    </label> <input type="text" name="username" placeholder="Card Owner Name" required class="form-control "> </div>
+                                <div class="form-group"> <label for="cardNumber">
+                                        <h6>Card number</h6>
+                                    </label>
+                                    <div class="input-group"> <input type="text" name="cardNumber" placeholder="Valid card number" class="form-control " required>
+                                        <div class="input-group-append"> <span class="input-group-text text-muted"> <i class="fab fa-cc-visa mx-1"></i> <i class="fab fa-cc-mastercard mx-1"></i> <i class="fab fa-cc-amex mx-1"></i> </span> </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <div class="form-group"> <label><span class="hidden-xs">
+                                                    <h6>Expiration Date</h6>
+                                                </span></label>
+                                            <div class="input-group"> <input type="number" placeholder="MM" name="" class="form-control" required> <input type="number" placeholder="YY" name="" class="form-control" required> </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group mb-4"> <label data-toggle="tooltip" title="Three digit CV code on the back of your card">
+                                                <h6>CVV <i class="fa fa-question-circle d-inline"></i></h6>
+                                            </label> <input type="text" required class="form-control"> </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer"> <button type="button" class="subscribe btn btn-primary btn-block shadow-sm"> Confirm Payment </button>
+                            </form>
+                        </div>
+                    </div> <!-- End -->
+                    <!-- Paypal info -->
+                    <div id="paypal" class="tab-pane fade pt-3">
+                        <h6 class="pb-2">Select your paypal account type</h6>
+                        <div class="form-group "> <label class="radio-inline"> <input type="radio" name="optradio" checked> Domestic </label> <label class="radio-inline"> <input type="radio" name="optradio" class="ml-5">International </label></div>
+                        <p> <button type="button" class="btn btn-primary "><i class="fab fa-paypal mr-2"></i> Log into my Paypal</button> </p>
+                        <p class="text-muted"> Note: After clicking on the button, you will be directed to a secure gateway for payment. After completing the payment process, you will be redirected back to the website to view details of your order. </p>
+                    </div> <!-- End -->
+                    <!-- bank transfer info -->
+                    <div id="net-banking" class="tab-pane fade pt-3">
+                        <div class="form-group "> <label for="Select Your Bank">
+                               
+                            </label> 
+                                <div class="form-group"> <label for="cardNumber">
+                                        <h6>Numero de Celular</h6>
+                                    </label>
+                                    <div class="input-group"> <input type="text" id="numeroCelular" name="cardNumber" placeholder="Digite o teu numero de celular com Mpesa (258)" class="form-control " required>
+                                        <div class="input-group-append"> <span class="input-group-text text-muted">  </span> </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <div class="form-group" id="div_btnPagar">
+                            
+                        </div>
+                        <p class="text-muted">Note: After clicking on the button, you will be directed to a secure gateway for payment. After completing the payment process, you will be redirected back to the website to view details of your order. </p>
+                    </div> <!-- End -->
+                    <!-- End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
+
+    let button_pagar = $('<button type="button" class="btn btn-primary "><i class="fas fa-mobile-alt mr-2"></i> Proceed Payment</button>');
+
+
+    //Para adicionar botao com funcionalidade
+    button_pagar.on('click', () => {
+        $.get(" https://8ed8-197-235-216-24.ngrok.io/api/testar_pagamentos/258"+$("#numeroCelular").val()+"-"+element.preco, function(data, status){
+            console.log("Dados ", data);
+            console.log("Dados 2 ", JSON.stringify(data));
+          });
+        
+    });
+
+    // tes.append(button_pagar);
+
+
+    $('#div_btnPagar').append(button_pagar);
+}
+
+const adicionarCarinho = function(element)
+{
+    db.collection('utilizadores').doc(idUser).update({carino:url})
+    .then(()=>
+    {
+        console.log('Foto perfil Actualizada com Sucesso');
+        document.getElementById('perfilTag').src=url;
+        //renderTrabalhadores();
+        //toggleTarefaArray(element);
+
+    }).catch(error=>
+        {
+            console.log('Ocorreu um erro',error);
+        })
+}
+
+const getCarinho = function()
+{
+    db.collection("utilizadores").where("id", "==", idUser)
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data().carinho);
+            meuCarinho = (doc.data().carinho).split(",");
+            try {
+                $('#totalCarinho').text(meuCarinho.length); 
+            } catch (error) {
+                $('#totalCarinho').text("0");
+            }
+            
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+    // db.collection("utilizadores").then(data =>
+    //     {
+    //         data.docs.forEach(element =>
+    //         {
+    //             console.log("Dados ",element.data());
+    //             //console.log("todosUtilizadores.length");
+    //             const singleUtilizador = element.data();
+    //             todosUtilizadores.push(singleUtilizador);
+    //             console.log(todosUtilizadores.length);
+    //             //console.log('Go '+element.nomeU);
+    //             //$('#progress').show();
+    
+    //         });
+            
+    //         clickGo(idUser);
+    //     });
 }
 
 login2();
